858b3e5418dbf9a19f2cbbb02626f407d36932c6
kylemnguyen <kylemtnguyen@gmail.com>
2026-02-20 15:54:54 -0700

    All of my current code, need to work on admin sign up

--- website/auth.py
+++ website/auth.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db   ##means from __init__.py import db
+from .models import Admin
 from flask_login import login_user, login_required, logout_user, current_user
 
 
@@ -18,7 +19,7 @@ def login():
             if check_password_hash(user.password, password): #Check the password against the hashed password
                 flash('Logged in successfully!', category='success') #Message
                 login_user(user, remember=True) #Use the flask_login functions to set the current user
-                return redirect(url_for('views.home')) #Redirect back to home
+                return redirect(url_for('admin.dashboard')) #Redirect back to home
             else:
                 flash('Incorrect password, try again.', category='error') #If password is wrong error message
         else:
@@ -42,7 +43,7 @@ def sign_up():
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
-        user = User.query.filter_by(email=email).first()
+        user = Admin.query.filter_by(email=email).first()
         if user:
             flash('Email already exists.', category='error')
         elif len(email) < 4:
@@ -54,12 +55,12 @@ def sign_up():
         elif len(password1) < 7:
             flash('Password must be at least 7 characters.', category='error')
         else:
-            new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1)) #Hash users password
+            new_user = Admin(email=email, first_name=first_name, password=generate_password_hash(password1)) #Hash users password
             db.session.add(new_user) #Add the new user to the database
             db.session.commit() #Save
             login_user(new_user, remember=True) #User flask to login new user
             flash('Account created!', category='success') #Message
-            return redirect(url_for('views.home')) #Redirect to home
+            return redirect(url_for('admin.dashboard')) #Redirect to home
 
     return render_template("admin/sign_up.html", user=current_user) #Render sign up page
 
--- website/models.py
+++ website/models.py
@@ -29,8 +29,8 @@ class Walker(db.Model):
 class Admin(db.Model): 
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True)
-    password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
+    password = db.Column(db.String(150))
 
 class Recurring(db.Model): 
     id = db.Column(db.Integer, primary_key=True)
