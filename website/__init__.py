37694f2896ecc9eca00b3e35bf938b564ca661c6
Kishan <prasadkishan2@outlook.com>
2026-02-20 13:37:12 -0700

    Got the maps.html working

--- website/__init__.py
+++ website/__init__.py
@@ -14,11 +14,11 @@ def create_app():
     app = Flask(__name__) #Create app
     app.config['SECRET_KEY'] = os.getenv("FLASK_SECRET_KEY")  #Secret key.
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}' #Location of the database. IE the same folder as parent 
-    app.config["MAPS_KEY"] = os.getenv("DIRECTIONS_API_KEY") #Maps API key
+    app.config["MAPS_KEY"] = os.getenv("MAPS_API_KEY") #Maps API key
 
     if not app.config['SECRET_KEY']:
         raise ValueError("SECRET_KEY not set")
-    if not app.config['API_KEY']:
+    if not app.config['MAPS_KEY']:
         raise ValueError("API_KEY not set")
 
     db.init_app(app) #Connect the database to the app
--- website/views.py
+++ website/views.py
@@ -2,16 +2,19 @@ from flask import Blueprint, render_template, request, flash, jsonify
 from flask_login import login_required, current_user
 from . import db
 import json
+from dotenv import load_dotenv 
+import os 
+load_dotenv() # Load variables from .env to environment
 
 views = Blueprint('views', __name__) #Create a blueprint for this files. IE the "views"
 
 @views.route("/")
 def home():
-    return render_template("home.html")@views.route("/map")
+    return render_template("home.html")
 
 @views.route("/map")
 def map():
     return render_template(
-        "map.html",
-        maps_key=views.config["MAPS_KEY"]
+        "maps.html",
+        maps_key=os.getenv("MAPS_API_KEY")
     )
