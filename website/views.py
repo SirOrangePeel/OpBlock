2d3310a63f76d5c6a483426c91cf5a5fb9e9ac70
Aaron Bedard <abedard2005@gmail.com>
2026-02-20 13:12:04 -0700

    Setup map template in website/views

--- website/views.py
+++ website/views.py
@@ -6,3 +6,9 @@ import json
 
 views = Blueprint('views', __name__) #Create a blueprint for this files. IE the "views"
 
+@views.route("/map")
+def map():
+    return render_template(
+        "map.html",
+        maps_key=current_app.config["MAPS_KEY"]
+    )
