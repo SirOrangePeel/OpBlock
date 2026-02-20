739b439755b36fd1f04ceededfbaa42f83a64075
Seth Close <seth@sethclose.au>
2026-02-20 12:55:04 -0700

    Fixed init for model code

--- website/__init__.py
+++ website/__init__.py
@@ -2,6 +2,9 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
+from dotenv import load_dotenv 
+import os 
+load_dotenv() # Load variables from .env to environment
 
 db = SQLAlchemy() #Start database object
 DB_NAME = "database.db" #Name of database
@@ -9,7 +12,7 @@ DB_NAME = "database.db" #Name of database
 
 def create_app():
     app = Flask(__name__) #Create app
-    app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs' #Secret key. Should be moved to a hidden .env file
+    app.config['SECRET_KEY'] = os.getenv("FLASK_SECRET_KEY")  #Secret key.
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}' #Location of the database. IE the same folder as parent 
     db.init_app(app) #Connect the database to the app
 
@@ -22,20 +25,20 @@ def create_app():
     app.register_blueprint(auth, url_prefix='/')
 
     #Import the database model schemas
-    # from .models import Walk, Walker, Active, History
+    from .models import Admin, Walk, Walker, Recurring, Active, History
     
     #Using the app context (The connected database and models) create the schemas
     create_database(app) #Creating the database
 
     #Start login manager
-    # login_manager = LoginManager() #Create object
-    # login_manager.login_view = 'auth.login' #View where users will login
-    # login_manager.init_app(app) #Connect login manager to app
+    login_manager = LoginManager() #Create object
+    login_manager.login_view = 'auth.login' #View where users will login
+    login_manager.init_app(app) #Connect login manager to app
 
     #User loader for login manager. Not quite sure what this is but I think it is the way that login manager should be able to access a user. IE the query to be able to get a User object
     @login_manager.user_loader
     def load_user(id):
-        return User.query.get(int(id))
+        return Admin.query.get(int(id))
 
     return app #Return the configured app
 
